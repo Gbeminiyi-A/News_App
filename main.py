f056a4ba2c6242726a11cacf4d6fa784873935c6
Oluwagbeminiyi <gagbedejobi@gmail.com>
2024-01-28 22:08:10 +0100

    created the search form and page in templates

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 year = datetime.datetime.now().year
 
 
+def search(keyword):
+    search_url = (f'https://newsapi.org/v2/top-headlines?'
+                  f'apiKey={NEWS_APIKEY}&q={keyword}')
+    search_response = requests.get(search_url)
+    search_articles = search_response.json()['articles']
+    return search_articles
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     url = ('https://newsapi.org/v2/top-headlines?'
@@ -19,47 +27,52 @@ def home_page():
     articles = response.json()['articles']
     if request.method == 'POST':
         keyword = request.form.get('search')
-        search_url = (f'https://newsapi.org/v2/top-headlines?'
-                      f'apiKey={NEWS_APIKEY}&q={keyword}')
-        search_response = requests.get(search_url)
-        search_articles = search_response.json()['articles']
+        search_articles = search(keyword)
         return render_template('search.html', search_articles=search_articles)
     return render_template('home.html', articles=articles, year=year, category='World')
 
 
-@app.route("/nigeria", methods=['GET', 'POST'])
-def nigerian_news():
-    url = ('https://newsapi.org/v2/top-headlines?'
-           f'apiKey={NEWS_APIKEY}&country=ng')
-    response = requests.get(url)
-    articles = response.json()['articles']
-    if request.method == 'POST':
-        keyword = request.form.get('search')
-        search_url = (f'https://newsapi.org/v2/top-headlines?'
-                      f'apiKey={NEWS_APIKEY}&q={keyword}')
-        search_response = requests.get(search_url)
-        search_articles = search_response.json()['articles']
-        if len(search_articles) == 0:
-            return "Nothing Found"
-        return render_template('search.html', search_articles=search_articles)
-    return render_template('nigeria.html', articles=articles)
+# @app.route("/nigeria", methods=['GET', 'POST'])
+# def nigerian_news():
+#     url = ('https://newsapi.org/v2/top-headlines?'
+#            f'apiKey={NEWS_APIKEY}&country=ng')
+#     response = requests.get(url)
+#     articles = response.json()['articles']
+#     if request.method == 'POST':
+#         keyword = request.form.get('search')
+#         search_url = (f'https://newsapi.org/v2/top-headlines?'
+#                       f'apiKey={NEWS_APIKEY}&q={keyword}')
+#         search_response = requests.get(search_url)
+#         search_articles = search_response.json()['articles']
+#         if len(search_articles) == 0:
+#             return "Nothing Found"
+#         return render_template('search.html', search_articles=search_articles)
+#     return render_template('nigeria.html', articles=articles)
 
 
-@app.route("/<category>")
+@app.route("/<category>", methods=['GET', 'POST'])
 def category_page(category):
     url = ('https://newsapi.org/v2/top-headlines?'
            f'apiKey={NEWS_APIKEY}&category={category}')
     response = requests.get(url)
     articles = response.json()['articles']
+    if request.method == 'POST':
+        keyword = request.form.get('search')
+        search_articles = search(keyword)
+        return render_template('search.html', search_articles=search_articles)
     return render_template('category.html', articles=articles, year=year, category=category.upper())
 
 
-@app.route("/<country>")
+@app.route("/<country>", methods=['GET', 'POST'])
 def country_page(country):
     url = ('https://newsapi.org/v2/top-headlines?'
            f'apiKey={NEWS_APIKEY}&country={country}')
     response = requests.get(url)
     articles = response.json()['articles']
+    if request.method == 'POST':
+        keyword = request.form.get('search')
+        search_articles = search(keyword)
+        return render_template('search.html', search_articles=search_articles)
     return render_template('category.html', articles=articles, year=year, category=country.upper())
 
 
