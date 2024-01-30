3c42441196ec98b8c11260bafa12e69a226866f6
Oluwagbeminiyi <gagbedejobi@gmail.com>
2024-01-30 23:15:12 +0100

    completed the search page and removed commented routes

--- main.py
+++ main.py
@@ -28,26 +28,8 @@ def home_page():
     if request.method == 'POST':
         keyword = request.form.get('search')
         search_articles = search(keyword)
-        return render_template('search.html', search_articles=search_articles)
+        return render_template('search.html', articles=search_articles, year=year)
     return render_template('home.html', articles=articles, year=year, category='World')
-
-
-# @app.route("/nigeria", methods=['GET', 'POST'])
-# def nigerian_news():
-#     url = ('https://newsapi.org/v2/top-headlines?'
-#            f'apiKey={NEWS_APIKEY}&country=ng')
-#     response = requests.get(url)
-#     articles = response.json()['articles']
-#     if request.method == 'POST':
-#         keyword = request.form.get('search')
-#         search_url = (f'https://newsapi.org/v2/top-headlines?'
-#                       f'apiKey={NEWS_APIKEY}&q={keyword}')
-#         search_response = requests.get(search_url)
-#         search_articles = search_response.json()['articles']
-#         if len(search_articles) == 0:
-#             return "Nothing Found"
-#         return render_template('search.html', search_articles=search_articles)
-#     return render_template('nigeria.html', articles=articles)
 
 
 @app.route("/<category>", methods=['GET', 'POST'])
@@ -59,7 +41,7 @@ def category_page(category):
     if request.method == 'POST':
         keyword = request.form.get('search')
         search_articles = search(keyword)
-        return render_template('search.html', search_articles=search_articles)
+        return render_template('search.html', articles=search_articles, year=year)
     return render_template('category.html', articles=articles, year=year, category=category.upper())
 
 
